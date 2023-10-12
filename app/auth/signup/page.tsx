890382859f472b63b7ca3980150a75efe67c42d5
Vaibhav Tiwari <vaibhav.tiwari@unilever.com>
2023-10-12 16:43:34 +0530

    lnding page finished

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
-
+import SignUpForm from "@/components/SignUpForm";
+import React, { FormEvent } from "react";
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="signup px-4 ">
+      <div className="heading mb-4 pt-12">
+        <h1 className="text-3xl bold text-lightYellow mb-2">
+          Create an account
+        </h1>
+        <p className="text-base">
+          Welcome to SWAP! Let us help you set up your own account and help set
+          up your goals. Let’s start with the basics.....
+        </p>
+      </div>
+      <SignUpForm />
+    </div>
+  );
 };
 
 export default Signup;
