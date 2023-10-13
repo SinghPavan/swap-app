d782e420a9167973e863641c515a617c90e85a40
aaychatt-admin <aayushman.chatterjee@publicissapient.com>
2023-10-13 17:21:56 +0530

    commiting stock jsons v2

---
+++ app/results/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductCard from "../../components/molecules/product-card/page";
+
+const results = () => {
+	return (
+		<div>
+			<ProductCard />
+		</div>
+	);
+};
+
+export default results;
