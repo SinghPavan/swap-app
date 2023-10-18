2e78f7075cc8dde0e080000d67f8efe5b1b56343
aaychatt-admin <aayushman.chatterjee@publicissapient.com>
2023-10-18 23:13:55 +0530

    hotfix demo

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -7,56 +7,55 @@ import React, { useEffect, useState } from "react";
 import { UserContext } from "@/utils/context";
 
 const Signup = () => {
-  const [userData, setUserData] = useState({
-    firstname: "",
-    lastname: "",
-  });
+	const [userData, setUserData] = useState({
+		firstname: "",
+		lastname: "",
+	});
 
-  useEffect(() => {
-    localStorage.clear();
-  }, []);
+	useEffect(() => {
+		localStorage.clear();
+	}, []);
 
-  return (
-    <UserContext.Provider value={{ userData, setUserData }}>
-      <div className="signup px-4 slide-in-element">
-        <div className="heading mb-8 pt-12">
-          <h1 className="text-3xl bold text-lightYellow mb-2">
-            Create an account
-          </h1>
-          <p className="text-base">
-            Welcome to SWAP! Let us help you set up your own account and help
-            set up your goals. Let’s start with the basics.....
-          </p>
-        </div>
-        <SignUpForm />
-        <p className="text-xl my-8 ">
-          <span className="font-bold">O</span>r
-        </p>
-        <SSO />
-        <Link href="/goals">
-          <button
-            type="button"
-            onClick={() => {
-              localStorage.setItem("userData", JSON.stringify(userData));
-              console.log("userData", userData);
-            }}
-            className="w-full flex justify-center mt-12"
-          >
-            <Image
-              src="/assets/next-btn.svg"
-              alt="next button"
-              width={50}
-              height={20}
-            />
-          </button>
-        </Link>
-        <div className="status flex items-center justify-center gap-2 mt-8">
-          <span className="bg-lightBlue h-1 w-12 block"></span>
-          <span className="bg-white h-1 w-12 block"></span>
-        </div>
-      </div>
-    </UserContext.Provider>
-  );
+	return (
+		<UserContext.Provider value={{ userData, setUserData }}>
+			<div className="signup px-4 slide-in-element">
+				<div className="heading mb-8 pt-12">
+					<h1 className="text-3xl bold text-lightYellow mb-2">
+						Create an account
+					</h1>
+					<p className="text-base">
+						Welcome to SWAP! Let us help you set up your own account and help
+						set up your goals. Let’s start with the basics.....
+					</p>
+				</div>
+				<SignUpForm />
+				<p className="text-xl my-8 ">
+					<span className="font-bold">Or</span>
+				</p>
+				<SSO />
+				<Link href="/goals">
+					<button
+						type="button"
+						onClick={() => {
+							localStorage.setItem("userData", JSON.stringify(userData));
+							console.log("userData", userData);
+						}}
+						className="w-full flex justify-center mt-12">
+						<Image
+							src="/assets/next-btn.svg"
+							alt="next button"
+							width={50}
+							height={20}
+						/>
+					</button>
+				</Link>
+				<div className="status flex items-center justify-center gap-2 mt-8">
+					<span className="bg-lightBlue h-1 w-12 block"></span>
+					<span className="bg-white h-1 w-12 block"></span>
+				</div>
+			</div>
+		</UserContext.Provider>
+	);
 };
 
 export default Signup;
--- components/appNavigation.tsx
+++ components/appNavigation.tsx
@@ -3,55 +3,55 @@ import Link from "next/link";
 import Image from "next/image";
 
 const AppNavigation = () => {
-  return (
-    <div className="app-navigation">
-      <div className="home-button">
-        <button className="app-navigation-button">
-          <Link href="/profile" role="button">
-            <div className="home-icon" />
-            Home
-          </Link>
-        </button>
-      </div>
-      <div className="products-button">
-        <button className="app-navigation-button">
-          <Link href="/product-listing" role="button">
-            <div className="products-icon" />
-            Products
-          </Link>
-        </button>
-      </div>
-      <div className="scan-button">
-        <button className="app-navigation-button">
-          <Link href="/scan" role="button">
-            {/* <div className="scan-icon" /> */}
-            <Image
-              src="/assets/scan-icon.svg"
-              alt="scan button"
-              width={65.92}
-              height={65.92}
-            />
-            Scan
-          </Link>
-        </button>
-      </div>
-      <div className="rewards-button">
-        <button className="app-navigation-button">
-          {/* <Link href="/rewards" role="button"> */}
-          <div className="rewards-icon" />
-          Rewards
-          {/* </Link> */}
-        </button>
-      </div>
-      <div className="profile-button">
-        <button className="app-navigation-button">
-          {/* <Link href="/profile" role="button"> */}
-          <div className="profile-icon" />
-          Profile
-          {/* </Link> */}
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="app-navigation">
+			<div className="home-button">
+				<button className="app-navigation-button">
+					<Link href="/profile" role="button">
+						<div className="home-icon" />
+						Home
+					</Link>
+				</button>
+			</div>
+			<div className="products-button">
+				<button className="app-navigation-button">
+					<Link href="/product-listing" role="button">
+						<div className="products-icon" />
+						Products
+					</Link>
+				</button>
+			</div>
+			<div className="scan-button">
+				<button className="app-navigation-button">
+					<Link href="/scan" role="button">
+						{/* <div className="scan-icon" /> */}
+						<Image
+							src="/assets/scan-icon.svg"
+							alt="scan button"
+							width={65.92}
+							height={65.92}
+						/>
+						SWAP
+					</Link>
+				</button>
+			</div>
+			<div className="rewards-button">
+				<button className="app-navigation-button">
+					{/* <Link href="/rewards" role="button"> */}
+					<div className="rewards-icon" />
+					Rewards
+					{/* </Link> */}
+				</button>
+			</div>
+			<div className="profile-button">
+				<button className="app-navigation-button">
+					{/* <Link href="/profile" role="button"> */}
+					<div className="profile-icon" />
+					Profile
+					{/* </Link> */}
+				</button>
+			</div>
+		</div>
+	);
 };
 export default AppNavigation;
