756adc36511c4050fd0a656cdecf8691811871e1
Vaibhav Tiwari <vaibhav.tiwari@unilever.com>
2023-10-18 08:48:53 +0530

    PWA

--- next.config.js
+++ next.config.js
@@ -14,16 +14,16 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-// const withPWA = require("next-pwa");
+const withPWA = require("next-pwa");
 
-// module.exports = withPWA({
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     skipWaiting: true,
-//   },
-//   // Other Next.js configuration options
-// });
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+  // Other Next.js configuration options
+});
 
 module.exports = {
   images: {
